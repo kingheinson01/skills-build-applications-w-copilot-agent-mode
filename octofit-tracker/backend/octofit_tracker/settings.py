a6ffc8f6e45fd54c57777d87c8cdce1c666d41c3
kingheinson01 <heinson.king@accenture.com>
2026-03-01 02:11:30 +0000

    codespace name for the URL endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,14 @@ SECRET_KEY = 'django-insecure--%+mc=)*m1u+$&uli6lez5fq@)*r1sd3j!hn@5(%bnmy7vpd63
 DEBUG = True
 
 
-# Allow all hosts for development
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
